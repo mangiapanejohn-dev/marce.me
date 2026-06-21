343b4d747912237a99c0031cd01f50c80c7fd734
1 <1>
2026-06-21 21:08:29 +0800

    fix: update X social link to correct handle

--- src/constants.ts
+++ src/constants.ts
@@ -25,7 +25,7 @@ export const SOCIALS: Social[] = [
   },
   {
     name: "X",
-    href: "https://x.com/moralesjavx1032",
+    href: "https://x.com/MarcEllingtonl",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
